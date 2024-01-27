53b1f942dbb586e804b431fc0f9029ee021a75dd
wononek <93595204+wononek@users.noreply.github.com>
2024-01-27 22:33:09 +0900

    Create 6068_35754924(AC).py

---
+++ 6068_35754924(AC).py
@@ -0,0 +1,14 @@
+﻿n = input()
+
+n = int(n)
+
+if n>=90:
+    print('A')
+else:
+    if n>=70:
+        print('B')
+    else:
+        if n>=40:
+            print('C')
+        else:
+            print('D')
